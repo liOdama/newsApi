5cfd30d7a82c96a647632fa5212cf41b8d20cf00
Roman Bychkov <romabrest1992@gmail.com>
2018-11-19 20:38:19 +0300

    feat: add class createElement

---
+++ src/createElement.js
@@ -0,0 +1,37 @@
+module.exports = class CreateFragment {
+    constructor() {
+      this.fragment = document.createDocumentFragment();
+      this.tempElement = undefined;
+    }
+  
+    createElement(nameTag) {
+      this.tempElement = document.createElement(`${nameTag}`);
+      return this;
+    }
+  
+    setAttr(attr) {
+      for (const key in attr) {
+        this.tempElement.setAttribute(key, attr[key]);
+      }
+      return this;
+    }
+  
+    setText(text) {
+      this.tempElement.innerHTML = text;
+      return this;
+    }
+  reInsert) {
+      if (whereInsert !== null) {
+        this.fragment.querySelector(whereInsert).appendChild(this.tempElement);
+      } else {
+        this.fragment.appendChild(this.tempElement);
+      }
+      return this;
+    }
+  
+    setInDocument(whereInsert) {
+      document.querySelector(whereInsert).appendChild(this.fragment);
+    }
+    setInFragment(whe
+  };
+  
---
+++ src/index.js
@@ -0,0 +1,11 @@
+let createElement = require('./createElement.js');
+
+window.onload = function(){
+ 
+  document.querySelector('.getRequest').addEventListener('click', sendRequest);
+
+  sendRequest(){
+    // отсылка запроса
+  }
+  
+};
